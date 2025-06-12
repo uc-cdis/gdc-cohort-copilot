1b852a6d1b6e4cec894fbc31244f745d5fd3d8a8
Steven Song <steven.song98@gmail.com>
2025-06-12 16:35:36 -0500

    cleanup

--- training/run.sh
+++ training/run.sh
@@ -1,20 +1,60 @@
 #!/bin/bash
 
+DATA_DIR=/opt/gpudata/gdc-cohort-data
+
 set -e  # Exit immediately if a command exits with a non-zero status
 
-echo "Starting SFT training..."
-python3 ./training/train.py --model mistralai/Mistral-7B-Instruct-v0.3 --input_csv /opt/gpudata/gdc-cohort-data/train.csv --output_dir /opt/gpudata/gdc-cohort-data/training_runs/sft --baseline_dir /opt/gpudata/gdc-cohort-data/baselines/sft --objective sft --batch_size 64 --max_epochs 1 --lr 5e-5 --max_seq_length 1024 --run_name users
+echo "Training Mistral"
+python 1-train.py \
+--model mistralai/Mistral-7B-Instruct-v0.3 \
+--input-csv $DATA_DIR/train.csv \
+--log-dir $DATA_DIR/logs/mistral \
+--output-dir $DATA_DIR/models/mistral \
+--batch-size 64 \
+--max-epochs 1 \
+--lr 5e-5 \
+--max-seq-length 1024
 
-echo "Finished SFT training. Starting Continual training..."
-python3 ./training/train.py --model gpt2 --input_csv /opt/gpudata/gdc-cohort-data/train.csv --output_dir /opt/gpudata/gdc-cohort-data/training_runs/continual --baseline_dir /opt/gpudata/gdc-cohort-data/baselines/continual --objective continual --batch_size 32 --max_epochs 10 --lr 5e-5 --max_seq_length 1024 --run_name users
+echo "Training GPT2"
+python 1-train.py \
+--model openai-community/gpt2 \
+--input-csv $DATA_DIR/train.csv \
+--log-dir $DATA_DIR/logs/gpt2 \
+--output-dir $DATA_DIR/models/gpt2 \
+--batch-size 32 \
+--max-epochs 10 \
+--lr 5e-5 \
+--max-seq-length 1024
 
-echo "Finished Continual training. Starting Seq2Seq training..."
-python3 ./training/train.py --model facebook/bart-base --input_csv /opt/gpudata/gdc-cohort-data/train.csv --output_dir /opt/gpudata/gdc-cohort-data/training_runs/seq2seq --baseline_dir /opt/gpudata/gdc-cohort-data/baselines/seq2seq --objective seq2seq --batch_size 32 --max_epochs 3 --lr 5e-5 --max_seq_length 1024 --run_name users
+echo "Training BART"
+python 1-train.py \
+--model facebook/bart-base \
+--input-csv $DATA_DIR/train.csv \
+--log-dir $DATA_DIR/logs/bart \
+--output-dir $DATA_DIR/models/bart \
+--batch-size 32 \
+--max-epochs 3 \
+--lr 5e-5 \
+--max-seq-length 1024
 
-echo "All initial training runs completed."
+echo "Training GPT2-100k"
+python 1-train.py \
+--model openai-community/gpt2 \
+--input-csv $DATA_DIR/train_synthetic_users+100k.csv \
+--log-dir $DATA_DIR/logs/gpt2-100k \
+--output-dir $DATA_DIR/models/gpt2-100k \
+--batch-size 32 \
+--max-epochs 10 \
+--lr 5e-5 \
+--max-seq-length 1024
 
-echo "Starting 100k synthetic training..."
-python3 ./training/train.py --model gpt2 --input_csv /opt/gpudata/gdc-cohort-data/train_synthetic_users+100k.csv --output_dir /opt/gpudata/gdc-cohort-data/training_runs/continual --baseline_dir /opt/gpudata/gdc-cohort-data/baselines/continual --objective continual --batch_size 32 --max_epochs 10 --lr 5e-5 --max_seq_length 1024 --run_name 100k
-
-echo "Finished 100k synthetic training. Starting 1M synthetic training.."
-python3 ./training/train.py --model gpt2 --input_csv /opt/gpudata/gdc-cohort-data/train_synthetic_users+1M.csv --output_dir /opt/gpudata/gdc-cohort-data/training_runs/continual --baseline_dir /opt/gpudata/gdc-cohort-data/baselines/continual --objective continual --batch_size 32 --max_epochs 10 --lr 5e-5 --max_seq_length 1024 --run_name 1M
+echo "Training GPT2-1M"
+python 1-train.py \
+--model openai-community/gpt2 \
+--input-csv $DATA_DIR/train_synthetic_users+1M.csv \
+--log-dir $DATA_DIR/logs/gpt2-1m \
+--output-dir $DATA_DIR/models/gpt2-1m \
+--batch_size 32 \
+--max_epochs 10 \
+--lr 5e-5 \
+--max_seq_length 1024
